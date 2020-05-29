eb4590edd9ca820f8717aa63619fd01dc55a2169
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-05-28 21:50:29 -0400

    Created character strings for all characters in password, defined generatePassword function to return lowerCase letter from string using math.random times length of string

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create character strings
+
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numbers = '0123456789'
+//const specialChar = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+
+// Define generatePassword function
+function generatePassword() {
+  //let length = 1;
+  let password = "";
+  let char = Math.floor(Math.random() * lowerCase.length);
+  console.log(char);
+  console.log(lowerCase.charAt(char));
+  password = lowerCase.charAt(char);
+  return password;
+}
+generatePassword();
+writePassword();
