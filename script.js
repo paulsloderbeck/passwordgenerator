ecf5af849cc306ae4423637de92dbd463721f9a1
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-05-30 16:20:52 -0400

    Added prompt for passwordLength, added for loop to generatePassword function

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
 
+// Define generatePassword function
+function generatePassword() {
+  let password = "";
+  for (let i = 0; i< passwordLength; i++) {
+  let char = Math.floor(Math.random() * lowerCase.length + 1);
+  console.log(char);
+  console.log(lowerCase.charAt(char));
+  password += lowerCase.charAt(char);
+  }
+  return password;
 }
 
 // Add event listener to generate button
@@ -20,15 +30,9 @@ const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numbers = '0123456789'
 //const specialChar = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
 
-// Define generatePassword function
-function generatePassword() {
-  //let length = 1;
-  let password = "";
-  let char = Math.floor(Math.random() * lowerCase.length);
-  console.log(char);
-  console.log(lowerCase.charAt(char));
-  password = lowerCase.charAt(char);
-  return password;
-}
-generatePassword();
+// Ask user for length of password
+let passwordLength = prompt("How long would you like your password to be? (8-128 characters please)");
+console.log("This is the password length " + passwordLength);
+
+
 writePassword();
